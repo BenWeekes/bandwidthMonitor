592ecf828a54e452c40f2a476c51db9c85fc8461
Ben Weekes <ben@agora.io>
2024-01-29 14:17:36 -0800

    o

--- capturev6.sh
+++ capturev6.sh
@@ -2,7 +2,7 @@
 
 echo "output to $HOME"
 
-/Applications/Wireshark.app/Contents/MacOS/tshark -a duration:1600 -B 4096 -i en0 -f "ip dst $1 or ip6 src $2" -T fields -e frame.time_relative -e frame.len -o data.show_as_text:TRUE -l  > "$HOME/tshark_in.txt" &
+/Applications/Wireshark.app/Contents/MacOS/tshark -a duration:1600 -B 4096 -i en0 -f "ip dst $1 or ip6 dst $2" -T fields -e frame.time_relative -e frame.len -o data.show_as_text:TRUE -l  > "$HOME/tshark_in.txt" &
 
 /Applications/Wireshark.app/Contents/MacOS/tshark -a duration:1600 -B 4096 -i en0 -f "ip src $1 or ip6 src $2" -T fields -e frame.time_relative -e frame.len -o data.show_as_text:TRUE -l  > "$HOME/tshark_out.txt" &
 
